ef211b77d6a98c0d33c23e87cf9aef20755a2157
chnspart <imchn24@gmail.com>
2024-10-06 17:06:25 -0400

    SalesFunnel motion added ++

--- components/pages/SalesFunnel.tsx
+++ components/pages/SalesFunnel.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React, { useRef, useMemo, Suspense, useState } from 'react';
+import React, { useRef, useMemo, Suspense, useState, useEffect } from 'react';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
 import { OrbitControls, useTexture, Svg } from '@react-three/drei';
 import * as THREE from 'three';
 import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader';
 import MultiStepModal from '../ui/MultiStepModal';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 // Define a type for the expected structure of the SVG data
 type SVGData = {
@@ -94,25 +96,90 @@ const Scene = () => (
 
 const SalesFunnel: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    }
+  }, [controls, inView]);
+
+  const containerVariants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.8,
+        ease: 'easeOut',
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 0.5, ease: 'easeOut' },
+    },
+  };
+
+  const buttonVariants = {
+    hidden: { scale: 0.8, opacity: 0 },
+    visible: { scale: 1, opacity: 1, transition: { duration: 0.5 } },
+    hover: { scale: 1.05, transition: { duration: 0.2 } },
+  };
 
   return (
-    <section className="relative w-full h-screen bg-gradient-to-br from-white-50 via-blue-50 to-purple-50 flex items-center justify-between px-8 overflow-hidden">
-      <div className="w-1/2 z-10 absolute bottom-10 mx-auto">
+    <section className="container relative w-full h-screen bg-gradient-to-br from-white-50 via-blue-50 to-purple-50 flex items-center justify-between px-8 overflow-hidden">
+      <motion.div
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={containerVariants}
+        className="w-1/2 z-10 absolute bottom-10 mx-auto"
+      >
         <div className="backdrop-filter backdrop-blur-lg bg-black/5 bg-opacity-10 p-8 rounded-xl">
-          <h2 className="text-blue-600 text-5xl font-bold mb-6 leading-tight">
-            Ready to convert? 
-          </h2>
-          <p className="text-gray-500 text-xl mb-8">
-            {"Revolutionize your outdoor advertising with our cutting-edge AI technology. Reach the right audience at the right time, maximizing your campaign's impact."}
-          </p>
-          <button 
+          <motion.h2 
+            variants={itemVariants} 
+            className="text-black text-5xl font-bold mb-6 leading-tight"
+          >
+            <motion.span
+              variants={itemVariants}
+              className='bg-gradient-to-b from-blue-700 to-slate-950 text-transparent bg-clip-text'
+            >
+              Ready
+            </motion.span>
+            {' '}to{' '}
+            <motion.span
+              variants={itemVariants}
+              className='bg-gradient-to-b from-orange-700 to-slate-950 text-transparent bg-clip-text'
+            >
+              Convert?
+            </motion.span>
+          </motion.h2>
+          <motion.p 
+            variants={itemVariants}
+            className="text-gray-500 text-xl mb-8"
+          >
+            Revolutionize your outdoor advertising with our cutting-edge AI technology. Reach the right audience at the right time, maximizing your campaign&apos;s impact.
+          </motion.p>
+          <motion.button 
+            variants={buttonVariants}
+            whileHover="hover"
             className="bg-blue-600 text-white px-8 py-4 rounded-lg text-xl font-semibold hover:bg-blue-700 transition-colors"
             onClick={() => setIsModalOpen(true)}
           >
             Book a free 10 min demo
-          </button>
+          </motion.button>
         </div>
-      </div>
+      </motion.div>
       <div className="w-full h-full">
         <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
           <Suspense fallback={null}>
